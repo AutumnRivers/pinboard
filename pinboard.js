03ec726a8386c8d910356c4128a5d36750ad40e2
Josh R <jprossela@gmail.com>
2018-01-30 21:13:18 -0500

    v1.0.2stable
    * Cooldown added
    * Handling images is better than ever before!
    * Invite command added. Within this version, it's universal, so it should work without you having to change anything if you use this in your own bot.

--- pinboard.js
+++ pinboard.js
@@ -4,6 +4,7 @@ const pinnedRecently = new Set();
 const http = require('http');
 const pind = require("./pind.json");
 const prefix = "pin.";
+const snekfetch = require("snekfetch");
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
@@ -17,8 +18,46 @@ setInterval(() => {
 
 client.login(pind.tokenboi)
 
-client.on("ready", () =>
-client.user.setGame("with pushpins"));
+client.on("ready", () => {
+client.user.setGame("with pushpins")
+  snekfetch.post(`https://ls.terminal.ink/api/v1/bots/380450195797835776`)
+.set('Authorization', pind.ink)
+  .send({ "server_count": client.guilds.size })
+  .then(() => console.log('Updated ls.terminal.ink stats.'))
+  .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+  snekfetch.post(`https://discordbots.org/api/bots/stats`)
+.set('Authorization', pind.dbots)
+  .send({ "server_count": client.guilds.size })
+  .then(() => console.log('Updated discordbots stats'))
+  .catch(err => console.error(`Error while posting to DiscordBots: ${err.body}`));
+  });
+
+client.on("guildCreate", () => {
+	snekfetch.post(`https://ls.terminal.ink/api/v1/bots/380450195797835776`)
+.set('Authorization', pind.ink)
+  .send({ "server_count": client.guilds.size })
+  .then(() => console.log('Updated ls.terminal.ink stats.'))
+  .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+  snekfetch.post(`https://discordbots.org/api/bots/stats`)
+.set('Authorization', pind.dbots)
+  .send({ server_count: client.guilds.size })
+  .then(() => console.log('Updated discordbots stats'))
+  .catch(err => console.error(`Error while posting to DiscordBots: ${err.body}`));
+  });
+  
+  client.on("guildDelete", () => {
+	snekfetch.post(`https://ls.terminal.ink/api/v1/bots/380450195797835776`)
+.set('Authorization', pind.ink)
+  .send({ "server_count": client.guilds.size })
+  .then(() => console.log('Updated ls.terminal.ink stats.'))
+  .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+  snekfetch.post(`https://discordbots.org/api/bots/stats`)
+.set('Authorization', pind.dbots)
+  .send({ server_count: client.guilds.size })
+  .then(() => console.log('Updated discordbots stats'))
+  .catch(err => console.error(`Error while posting to DiscordBots: ${err.body}`));
+  });
+  
 
 client.on("channelPinsUpdate", channel => {
 	if(pinnedRecently.has(channel.id)) return;
@@ -29,6 +68,11 @@ const board = channel.guild.channels.find("name", "pinboard")
 	const pinMsgs = messages.first()
 	
 	if(!pinMsgs) return;
+	if(pinMsgs.attachments.first() == undefined) {
+		var atch = undefined
+		} else {
+		var atch = pinMsgs.attachments.first().url
+		}
 	
 	if(!board) {
 if(channel.guild.me.hasPermission("MANAGE_CHANNELS") == false) return;
@@ -37,7 +81,7 @@ channel.guild.channels.find("name", "pinboard").send(pinMsgs.id, {embed:
 {color: 0x123456,
 title: `New pinned message in ${channel.name}`,
 description: pinMsgs.content}});
-if(ch.permissionsFor(message.guild.me).has("MANAGE_MESSAGES") == false) return;
+if(ch.permissionsFor(ch.guild.me).has("MANAGE_MESSAGES") == false) return;
 pinMsgs.unpin()
 pinnedRecently.add(ch.id);
 setTimeout(() => {
@@ -48,13 +92,17 @@ setTimeout(() => {
 board.send(pinMsgs.id, {embed:
 {color: 0x123456,
 title: `New pinned message in ${channel.name}`,
-description: pinMsgs.content}})
-if(ch.permissionsFor(message.guild.me).has("MANAGE_MESSAGES") == false) return;
+description: (pinMsgs.content),
+image: {
+url: atch
+}
+}})
+if(ch.permissionsFor(ch.guild.me).has("MANAGE_MESSAGES") == false) return;
 pinMsgs.unpin()
 pinnedRecently.add(ch.id);
 setTimeout(() => {
 	pinnedRecently.delete(ch.id);
-}, 2500);
+}, 45000);
 }
 	
 	if(ch.guild.id !== pind.testServer) return;
@@ -79,6 +127,8 @@ process.exit()
 if(message.content.toLowerCase() == prefix + "check") {
 	if(!boardCh) return message.channel.send("Channel check failed! Please create a #pinboard channel, or run pin.board, and let me make it for you!");
 	if(message.channel.permissionsFor(message.guild.me).has("MANAGE_MESSAGES") == false) return   message.channel.send("I do not have the manage messages permission for this guild! Please contact a server admin.");
+  
+  if(boardCh.permissionsFor(message.guild.me).has("SEND_MESSAGES") == false) return message.channel.send("Check failed! I do not have the send messages permission in the pinboard channel!");
 	if(message.guild.me.hasPermission("EMBED_LINKS") == false) return message.channel.send("I do not have the embed links permission for this guild! Please contact a server admin.");
 	message.channel.send("Check passed. I'm ready to start! :wave:");
 	}
@@ -95,5 +145,9 @@ if(message.content.toLowerCase() == prefix + "check") {
 if(message.content.toLowerCase() == prefix + "help") {
 	message.channel.send("**Pinboard Help**\n```\nPrefix: pin.\n\ninfo - Uhhhh stuff, I guess?\ncheck - Is Pinboard ready to go?\nboard - Create the #pinboard channel, if it doesn't exist already.\n```")
 	}
+  
+  if (message.content == ("pin.invite")) {
+message.channel.send(`Invite me to your server with this link!\nhttps://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=289808`)
+}
 	
 	})
